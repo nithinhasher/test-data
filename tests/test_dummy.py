872d0b26b991925aca059f18465d64d89ea32235
nithinhasher <mrl3@deloitte.com>
2023-11-18 14:33:45 +0530

    test

---
+++ tests/test_dummy.py
@@ -0,0 +1,16 @@
+import pytest
+
+def test_dummy_case_1():
+    assert True, f"TEST PASS"
+
+def test_dummy_case_2():
+    assert True, f"TEST PASS"
+
+def test_dummy_case_3():
+    assert True, f"TEST PASS"
+
+def test_dummy_case_4():
+    assert True, f"TEST PASS"
+
+def test_dummy_case_5():
+    assert True, f"TEST PASS"
---
+++ tests/test_scripts.py
@@ -0,0 +1,16 @@
+import pytest
+
+def test_scripts_case_1():
+    assert True, f"TEST PASS"
+
+def test_scripts_case_2():
+    assert True, f"TEST PASS"
+
+def test_scripts_case_3():
+    assert True, f"TEST PASS"
+
+def test_scripts_case_4():
+    assert False, f"TEST FAIL"
+
+def test_scripts_case_5():
+    assert True, f"TEST PASS"
